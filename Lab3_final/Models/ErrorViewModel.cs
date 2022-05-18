fec3be1ca856e7b8c150bea47b8cefb2d6180008
YanjunYu-1 <y9y9j8@163.com>
2022-05-18 10:31:54 -0500

    添加项目文件。

---
+++ Lab3_final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab3_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
